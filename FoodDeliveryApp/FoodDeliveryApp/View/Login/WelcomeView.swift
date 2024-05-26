cbe03af0cd8adbdfa0c65a65b6c644b4c250a87e
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-05-26 20:18:44 +0300

    welcome view
    completely finished the screen

--- FoodDeliveryApp/FoodDeliveryApp/View/Login/WelcomeView.swift
+++ FoodDeliveryApp/FoodDeliveryApp/View/Login/WelcomeView.swift
@@ -10,15 +10,46 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         ZStack {
+            Color.green
             VStack {
-                Image("")
+                Spacer()
+                Image("app_logo")
                     .resizable()
                     .scaledToFill()
+                    .frame(width: 90, height: 90)
                 
-                Text("")
+                Text("Food\nDelivery")
+                    .font(.customfont(.semibold, fontSize: 48))
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 20)
                 
+                Text("Заказ продуктов прямо домой")
+                    .font(.customfont(.medium, fontSize: 16))
+                    .foregroundColor(.white.opacity(0.7))
+                    .multilineTextAlignment(.center)
+                    //.padding(.top, 8)
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Text("Начать")
+                        .font(.customfont(.semibold, fontSize: 20))
+                        .foregroundStyle(.green)
+                        .multilineTextAlignment(.center)
+                }
+                .frame(maxWidth: .infinity, maxHeight: 60)
+                .background(Color.white)
+                .cornerRadius(25)
+                
+                Spacer()
+                    .frame(height: 60)
             }
+            
+            .padding(.horizontal, 20)
         }
+        .navigationTitle("")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
         .ignoresSafeArea()
     }
 }
