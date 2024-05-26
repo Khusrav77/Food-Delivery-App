fc5dead621a2f422e4efa5a2d278dbb2137321a4
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-05-26 17:29:18 +0300

    Added UIextension

---
+++ FoodDeliveryApp/FoodDeliveryApp/View/Login/WelcomeView.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeView.swift
+//  FoodDeliveryApp
+//
+//  Created by Khusrav Safiev on 5/26/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Image("")
+                    .resizable()
+                    .scaledToFill()
+                
+                Text("")
+                
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
