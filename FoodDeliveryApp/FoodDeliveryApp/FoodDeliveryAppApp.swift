0657c752447e17195d3a7741c67fc4170815751d
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-05-26 12:28:37 +0300

    Created base project, removed references

---
+++ FoodDeliveryApp/FoodDeliveryApp/FoodDeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryAppApp.swift
+//  FoodDeliveryApp
+//
+//  Created by Khusrav Safiev on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
