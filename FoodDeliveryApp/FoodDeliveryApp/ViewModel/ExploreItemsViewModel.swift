eb6dd6f5d6783508e3f911022f84e91ea68a4b19
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-06-17 16:02:36 +0300

    added exploreItemsVM

--- FoodDeliveryApp/FoodDeliveryApp/ViewModel/ExploreItemsViewModel.swift
+++ FoodDeliveryApp/FoodDeliveryApp/ViewModel/ExploreItemsViewModel.swift
@@ -10,14 +10,40 @@ import Foundation
 
 final class ExploreItemsViewModel: ObservableObject {
     
-    @Published var expItemsObj: ExploreCategoryModel = ExploreCategoryModel(dict: [:])
+    @Published var expItems: ExploreCategoryModel = ExploreCategoryModel(dict: [:])
     @Published var showError = false
     @Published var errorMessage = ""
     
-    @Published var listProductItem: [ProductModel] = []
+    @Published var listItems: [ProductModel] = []
     
     
-    init(expItemsObj: ExploreCategoryModel) {
-        self.expItemsObj = expItemsObj
+    init(expItems: ExploreCategoryModel) {
+        self.expItems = expItems
     }
+    
+    
+    //MARK: - SERVICE CALL
+    
+    func serviceCallList() {
+        ProductServiceCall.post(parameter: ["cat_id": self.expItems.id], path: Globals.SV_EXPLORE_ITEMS_LIST, isToken: true) { responseObj in
+            if let response = responseObj as? NSDictionary {
+                if response.value(forKey: KKey.status) as? String ?? "" == "1" {
+                    self.listItems = (response.value(forKey: KKey.payload)) as? NSArray ?? [])
+                        .map({list in
+                            return ProductModel(dict: list as? NSDictionary ?? [:])
+                            
+                        })
+                } else {
+                    self.errorMessage = response.value(forKey: KKey.message) as? String ?? "Fail"
+                    self.showError = true
+                }
+            }
+        } failure: { error in
+            self.errorMessage = error?.localizedDescription ?? "Fail"
+            self.showError = true
+        }
+
+    }
+    
+    
 }
