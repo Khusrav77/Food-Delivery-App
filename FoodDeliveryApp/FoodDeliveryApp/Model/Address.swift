405d40aa3d2c81e83c4d574a46b32de88768b9b8
Khusrav Safiev <147575346+Khusrav77@users.noreply.github.com>
2024-09-22 18:02:08 +0300

    update some code

--- FoodDeliveryApp/FoodDeliveryApp/Model/Address.swift
+++ FoodDeliveryApp/FoodDeliveryApp/Model/Address.swift
@@ -8,28 +8,18 @@
 import Foundation
 
 
-struct Address: Identifiable, Equatable {
+struct Address: Identifiable, Equatable, Codable {
+    var id: Int
+    var name: String
+    var phone: String
+    var address: String
+    var city: String
+    var state: String
+    var typeName: String
+    var postalCode: String
+    var isDefault: Int
     
-    var id: Int = 0
-    var name: String = ""
-    var phone: String = ""
-    var address: String = ""
-    var city: String = ""
-    var state: String = ""
-    var typeName: String = ""
-    var postalCode: String = ""
-    var isDefault: Int = 0
-    
-    
-    init(dict: NSDictionary) {
-        self.id = dict.value(forKey: "address_id") as? Int ?? 0
-        self.name = dict.value(forKey: "name") as? String ?? ""
-        self.phone = dict.value(forKey: "phone") as? String ?? ""
-        self.address = dict.value(forKey: "address") as? String ?? ""
-        self.city = dict.value(forKey: "city") as? String ?? ""
-        self.state = dict.value(forKey: "state") as? String ?? ""
-        self.typeName = dict.value(forKey: "type_name") as? String ?? ""
-        self.postalCode = dict.value(forKey: "postal_code") as? String ?? ""
-        self.isDefault = dict.value(forKey: "is_default") as? Int ?? 0
+    static func == (lhs: Address, rhs: Address) -> Bool {
+        return lhs.id == rhs.id
     }
 }
